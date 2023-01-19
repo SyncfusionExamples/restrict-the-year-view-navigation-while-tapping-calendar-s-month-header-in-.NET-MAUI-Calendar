7359e8956a9832445a901d5fc02d7086b739be57
VishalOmprasad <vishal.omprasad@syncfusion.com>
2023-01-19 23:20:07 +0530

    New KB sample added

---
+++ RestrictYearViewNavigation/RestrictYearViewNavigation/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace RestrictYearViewNavigation;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ RestrictYearViewNavigation/RestrictYearViewNavigation/Behavior/CalendarBehavior.cs
@@ -0,0 +1,46 @@
+﻿using Syncfusion.Maui.Calendar;
+
+namespace RestrictYearViewNavigation
+{
+    public class CalendarBehavior : Behavior<ContentPage>
+    {
+        private SfCalendar calendar;
+        private Button allowNavigationButton;
+
+        protected override void OnAttachedTo(ContentPage bindable)
+        {
+            base.OnAttachedTo(bindable);
+            this.calendar = bindable.FindByName<SfCalendar>("calendar");
+            this.calendar.AllowViewNavigation = true;
+            this.allowNavigationButton = bindable.FindByName<Button>("allowNavigationButton");
+            this.allowNavigationButton.Text = this.calendar.AllowViewNavigation ? "Disable view navigation" : "Enable view navigation";
+            this.allowNavigationButton.Clicked += AllowNavigationButton_Clicked;
+        }
+
+        private void AllowNavigationButton_Clicked(object sender, EventArgs e)
+        {
+            if (this.calendar.AllowViewNavigation)
+            {
+                this.calendar.AllowViewNavigation = false;
+                this.allowNavigationButton.Text = "Enable view navigation";
+            }
+            else
+            {
+                this.calendar.AllowViewNavigation = true;
+                this.allowNavigationButton.Text = "Disable view navigation";
+            }
+        }
+
+        protected override void OnDetachingFrom(ContentPage bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            if (this.allowNavigationButton != null)
+            {
+                this.allowNavigationButton.Clicked -= AllowNavigationButton_Clicked;
+            }
+
+            this.allowNavigationButton = null;
+            this.calendar = null;
+        }
+    }
+}
